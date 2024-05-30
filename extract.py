533e11c206e368225c458bcb2421e3715cd385b2
krysperz2 <krysperz2official@gmail.com>
2024-05-30 04:06:55 +0000

    downloaded songs

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 import csv, json
+import langdetect
 
 # with open("songs.jsonl") as f:
 #     with open("songs.csv", "w") as f2:
@@ -12,15 +13,31 @@ import csv, json
 #                              data["album"]["name"],
 #                              data["album"]["release_date"]])
             
-seed_tracks = []
-with open("songs2.jsonl") as f:
-    for d in f:
-        data = json.loads(d)
-        seed_tracks.append(data["id"]+" "+data["name"]+"\n")
+# seed_tracks = []
+# with open("songs.jsonl") as f:
+#     for d in f:
+#         data = json.loads(d)
+#         seed_tracks.append(data["id"]+" "+data["name"]+"\n")
 
-with open("seed-tracks", "w") as f:
-    f.writelines(seed_tracks)
+# with open("seed-tracks2", "w") as f:
+#     f.writelines(seed_tracks)
 
+# from transformers import pipeline
+
+# model_ckpt = "papluca/xlm-roberta-base-language-detection"
+# pipe = pipeline("text-classification", model=model_ckpt)
+
+# freq = {}
+# with open("seed-tracks2") as f:
+#     names = [l.split()[0] for l in f]
+#     langs = pipe(names)
+#     langs = [lang["label"] for lang in langs]
+#     freq = {lang:langs.count(lang) for lang in set(langs)}
+
+# for k,v in freq.items():
+#     print(k,":",v)
+
+    
 # with open("songs.csv") as f:
 #     reader = csv.reader(f)
 #     song_count, artist_count, max_artists = 0,0,0
@@ -30,3 +47,18 @@ with open("seed-tracks", "w") as f:
 #         artist_count += artists
 #         max_artists = artists if artists > max_artists else max_artists
 #     print(artist_count/song_count, max_artists, song_count)
+
+# songs = []
+# with open("songs.jsonl") as f:
+#     for l in f:
+#         data = json.loads(l)
+#         name = data["name"] + " | " + ", ".join([artist["name"] for artist in data["artists"]])
+#         songs.append(name)
+
+# with open("song-names", "w") as f:
+#     f.write("\n".join(songs))
+
+import os
+with open("song-names2") as f:
+    for s in f:
+        os.system(f"yt-dlp -x -q -S +size --download-archive ./music/songlist --max-filesize \"10M\" -o \"./music/%(title)s.%(ext)s\" \"ytsearch:{s}\"")
